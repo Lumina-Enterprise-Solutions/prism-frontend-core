64c5848f74a02ec4f7f9768235b1b82b838639de
Kristianto Budi Purwoko <kristiantobudipurwoko@gmail.com>
2025-05-25 18:42:49 +0700

    restore playwright, vite/browser

--- vitest.workspace.ts
+++ vitest.workspace.ts
@@ -1,14 +1,14 @@
-// import path from 'node:path';
-// import { fileURLToPath } from 'node:url';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 
 import { defineWorkspace } from 'vitest/config';
 
-// import { storybookTest } from '@storybook/experimental-addon-test/vitest-plugin';
+import { storybookTest } from '@storybook/experimental-addon-test/vitest-plugin';
 
-// const dirname =
-//   typeof __dirname !== 'undefined'
-//     ? __dirname
-//     : path.dirname(fileURLToPath(import.meta.url));
+const dirname =
+  typeof __dirname !== 'undefined'
+    ? __dirname
+    : path.dirname(fileURLToPath(import.meta.url));
 
 // More info at: https://storybook.js.org/docs/writing-tests/test-addon
 export default defineWorkspace([
@@ -18,7 +18,7 @@ export default defineWorkspace([
     plugins: [
       // The plugin will run tests for the stories defined in your Storybook config
       // See options at: https://storybook.js.org/docs/writing-tests/test-addon#storybooktest
-      // storybookTest({ configDir: path.join(dirname, '.storybook') }),
+      storybookTest({ configDir: path.join(dirname, '.storybook') }),
     ],
     test: {
       name: 'storybook',
